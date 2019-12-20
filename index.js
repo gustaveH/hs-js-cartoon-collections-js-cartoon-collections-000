0fe9fe47447b9c251136c3cd239444fac17beb77
Gustave Habimana <ghabiman@gmail.com>
2019-12-20 14:07:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var string = [];
+	dwarves.forEach(function(name, i) {
+		string.push(i+1 +". " + name + " ");
+	});
+	return string.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = [];
+    planeteerCalls.forEach(function(element) {
+  	element = element.toUpperCase() + "!";
+  	calls.push(element);
+  });
+  return calls;
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ["mozarella", "cheddar", "gouda"];
+	for(var i = 0; i < foods.length; i++) {
+		var cheeseIndex = cheese.indexOf(foods[i]);
+		if(cheeseIndex !== -1) {
+			return foods[i];
+		}
+	}
+	return "no cheese!";
 }
