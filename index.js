c790b874f3f880db6e09e219616b560f21f3251f
Gustave Habimana <ghabiman@gmail.com>
2019-12-20 17:56:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//function that accepts dwarf names and returns them numbered in a string
+
 function dwarfRollCall(dwarves) {
   var string = [];
-	dwarves.forEach(function(name, i) {
+	dwarves.forEach(function(name, i) {            
 		string.push(i+1 +". " + name + " ");
 	});
 	return string.join("");
 }
 
+//function should accept an array then convert each element to toUpperCase
 function summonCaptainPlanet(planeteerCalls){
   var calls = [];
     planeteerCalls.forEach(function(element) {
@@ -14,6 +17,8 @@ function summonCaptainPlanet(planeteerCalls){
   });
   return calls;
 }
+
+//function should accept array calls, then tell whether words are longer than 4 characters
 
 function longPlaneteerCalls(words) {
   for(var i = 0; i < words.length; i++) {
@@ -24,8 +29,10 @@ function longPlaneteerCalls(words) {
 	return false;
 }
 
+//function should accept array of strings, look through them, and then return the first string hat is a type of cheese
+
 function findTheCheese (foods) {
-  var cheese = ["mozarella", "cheddar", "gouda"];
+  var cheese = ["camembert", "cheddar", "gouda"];
 	for(var i = 0; i < foods.length; i++) {
 		var cheeseIndex = cheese.indexOf(foods[i]);
 		if(cheeseIndex !== -1) {
